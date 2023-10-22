75e71d9c3b8e7f6cb7cf3005d6f6604c26590784
ErikaD92 <erika.dlgr@gmail.com>
2023-10-22 11:40:39 +0200

    added the functionality of the select element

--- src/index.js
+++ src/index.js
@@ -1,39 +1,63 @@
 function updateTime() {
   //Vancouver
   let vancouverElement = document.querySelector("#vancouver");
-  let vancouverDateElement = vancouverElement.querySelector(".date");
-  let vancouverTimeElement = vancouverElement.querySelector(".time");
-  let vancouverTime = moment().tz("America/Vancouver");
+  if (vancouverElement) {
+    let vancouverDateElement = vancouverElement.querySelector(".date");
+    let vancouverTimeElement = vancouverElement.querySelector(".time");
+    let vancouverTime = moment().tz("America/Vancouver");
 
-  vancouverDateElement.innerHTML = vancouverTime.format("MMMM Do YYYY");
-  vancouverTimeElement.innerHTML = vancouverTime.format("HH:mm:ss");
-
+    vancouverDateElement.innerHTML = vancouverTime.format("MMMM Do YYYY");
+    vancouverTimeElement.innerHTML = vancouverTime.format("HH:mm:ss");
+  }
   //Buenos Aires
   let buenosAiresElement = document.querySelector("#buenos-aires");
-  let buenosAiresDateElement = buenosAiresElement.querySelector(".date");
-  let buenosAiresTimeElement = buenosAiresElement.querySelector(".time");
-  let buenosAiresTime = moment().tz("America/Argentina/Buenos_Aires");
+  if (buenosAiresElement) {
+    let buenosAiresDateElement = buenosAiresElement.querySelector(".date");
+    let buenosAiresTimeElement = buenosAiresElement.querySelector(".time");
+    let buenosAiresTime = moment().tz("America/Argentina/Buenos_Aires");
 
-  buenosAiresDateElement.innerHTML = buenosAiresTime.format("MMMM Do YYYY");
-  buenosAiresTimeElement.innerHTML = buenosAiresTime.format("HH:mm:ss");
-
+    buenosAiresDateElement.innerHTML = buenosAiresTime.format("MMMM Do YYYY");
+    buenosAiresTimeElement.innerHTML = buenosAiresTime.format("HH:mm:ss");
+  }
   //Rome
   let romeElement = document.querySelector("#rome");
-  let romeDateElement = romeElement.querySelector(".date");
-  let romeTimeElement = romeElement.querySelector(".time");
-  let romeTime = moment().tz("Europe/Rome");
+  if (romeElement) {
+    let romeDateElement = romeElement.querySelector(".date");
+    let romeTimeElement = romeElement.querySelector(".time");
+    let romeTime = moment().tz("Europe/Rome");
 
-  romeDateElement.innerHTML = romeTime.format("MMMM Do YYYY");
-  romeTimeElement.innerHTML = romeTime.format("HH:mm:ss");
-
+    romeDateElement.innerHTML = romeTime.format("MMMM Do YYYY");
+    romeTimeElement.innerHTML = romeTime.format("HH:mm:ss");
+  }
   //Sydney
   let sydneyElement = document.querySelector("#sydney");
-  let sydneyDateElement = sydneyElement.querySelector(".date");
-  let sydneyTimeElement = sydneyElement.querySelector(".time");
-  let sydneyTime = moment().tz("Australia/Sydney");
+  if (sydneyElement) {
+    let sydneyDateElement = sydneyElement.querySelector(".date");
+    let sydneyTimeElement = sydneyElement.querySelector(".time");
+    let sydneyTime = moment().tz("Australia/Sydney");
 
-  sydneyDateElement.innerHTML = sydneyTime.format("MMMM Do YYYY");
-  sydneyTimeElement.innerHTML = sydneyTime.format("HH:mm:ss");
+    sydneyDateElement.innerHTML = sydneyTime.format("MMMM Do YYYY");
+    sydneyTimeElement.innerHTML = sydneyTime.format("HH:mm:ss");
+  }
 }
+
+function updateCity(event) {
+  let citiesElement = document.querySelector("#cities");
+  let cityTimezone = event.target.value;
+  let cityTime = moment().tz(cityTimezone);
+  let cityName = cityTimezone.replace("_", " ").split("/")[1];
+  citiesElement.innerHTML = `
+  <div class="city">
+    <div>
+      <h2>${cityName}</h2>
+      <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+    </div>
+    <div class="time">${cityTime.format("HH:mm:ss")}</div>
+  </div>`;
+}
+
 updateTime();
 setInterval(updateTime, 1000);
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
